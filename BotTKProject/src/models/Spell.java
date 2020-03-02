67121b50bc15d9758fad8ddda05db0d34c327e06
bekahmark12 <rebekahmark12@gmail.com>
2020-03-02 15:38:49 -0700

    Personal Notes

--- BotTKProject/src/models/Spell.java
+++ BotTKProject/src/models/Spell.java
@@ -6,10 +6,26 @@ public class Spell {
     private final int affectRating;
     private final SpellType type;
 
+    public int getCost() {
+        return cost;
+    }
+
+    public int getIdealRange() {
+        return idealRange;
+    }
+
+    public int getAffectRating() {
+        return affectRating;
+    }
+
+    public SpellType getType() {
+        return type;
+    }
+
     public Spell(int cost, int idealRange, int affectRating, SpellType type) {
-        this.cost = cost;
-        this.idealRange = idealRange;
-        this.affectRating = affectRating;
-        this.type = type;
+        this.cost = getCost();
+        this.idealRange = getIdealRange();
+        this.affectRating = getAffectRating();
+        this.type = getType();
     }
 }
