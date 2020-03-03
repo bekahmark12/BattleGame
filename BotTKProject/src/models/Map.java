8dc2d5523071e84c671721a85a24e7c4c440d845
Techno1919 <61212536+Techno1919@users.noreply.github.com>
2020-03-03 14:49:43 -0700

    Updated UML and project

--- BotTKProject/src/models/Map.java
+++ BotTKProject/src/models/Map.java
@@ -1,16 +1,66 @@
 package models;
 
+import java.util.Random;
+
 public class Map {
-    private int row;
-    private int col;
-    private Icons[][] grid = new Icons[25][20];
+    private static Icons[][] grid = new Icons[12][24];
+    private static Random rng = new Random();
 
-    public void populateMap(Icons[][] grid){
+//    public void run(){
+//        printBoard(randomMap());
+//    }
 
+    private static Icons[][] randomMap(){
+        int randomE;
+        int randomP;
+
+        Icons obstacle = Icons.T;
+        for(int o = 40; o >= 0; o--){
+            int randomRow = rng.nextInt(12);
+            int randomCols = rng.nextInt(24);
+            if(Map.checkValidSpace(randomRow, randomCols)){
+                grid[randomRow][randomCols] = obstacle;
+                if(obstacle == Icons.T){
+                    obstacle = Icons.O;
+                }else{
+                    obstacle = Icons.T;
+                }
+            }
+        }
+        do{
+            randomE = rng.nextInt(12);
+        }while (!Map.checkValidSpace(randomE, 23));
+        grid[randomE][23] = Icons.E;
+
+        do{
+            randomP = rng.nextInt(12);
+        }while (!Map.checkValidSpace(randomP, 23));
+        grid[randomP][0] = Icons.P;
+
+        return grid;
     }
 
-    public boolean checkValidSpace(int row, int cols){
-        throw new UnsupportedOperationException("This has not yet been implemented.");
+    public static boolean checkValidSpace(int row, int cols){
+        boolean isInvalid = false;
+        if(grid[row][cols] == null || grid[row][cols] == Icons._){
+            isInvalid = true;
+        }
+        return isInvalid;
+    }
+
+
+    public static void printBoard(Icons[][] board){
+        for(Icons[] icons : board){
+            for (Icons icon : icons){
+                if(icon == null){
+                    System.out.printf("%-3s", Icons._);
+                }
+                else{
+                    System.out.printf("%-3s", icon);
+                }
+            }
+            System.out.println();
+        }
     }
 
     public Icons[][] getGrid() {
--- BotTKProject/src/prog/Main.java
+++ BotTKProject/src/prog/Main.java
@@ -1,8 +1,9 @@
 package prog;
 
+import models.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
     }
 }
