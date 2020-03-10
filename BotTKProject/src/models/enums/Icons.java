75ee1ed7eab0a010b2ff67f15f740ebf70be34f9
Techno1919 <61212536+Techno1919@users.noreply.github.com>
2020-03-10 14:06:07 -0600

    Changed Icons

--- BotTKProject/src/models/enums/Icons.java
+++ BotTKProject/src/models/enums/Icons.java
@@ -1,15 +1,10 @@
 package models.enums;
 
 public enum Icons {
-    P(0),
-    E(5),
-    T(3),
-    O(1),
-    _(0);
+    A,
+    B,
+    T,
+    O,
+    _
 
-    public final int cost;
-
-    private Icons(int cost) {
-        this.cost = cost;
-    }
 }
